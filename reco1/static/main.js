85bb2cee1eff3846c17f1ac224b721f6f9b8e26c
Beau Cronin <beau@priorknowledge.net>
2012-06-27 14:20:44 -0700

    cleans up query construction

--- reco1/static/main.js
+++ reco1/static/main.js
@@ -1,3 +1,28 @@
+function getRatings() {
+  // construct the query by collecting all of the observed ratings, and by
+  // marking all of the un-rated items with null
+  ratings_query = {}
+  $('input[type=radio].first-star').each(function(i, x) {
+    movie_id = x.name.split('_')[0]
+    num_stars = $(x).data('rating').rater.children().filter('.star-rating-on').length
+    if (num_stars === 0) {
+      ratings_query[movie_id] = null;
+    } else {
+      ratings_query[movie_id] = String(num_stars)
+    }
+  });
+
+  // make the call to the server to get the recommendations
+  $.ajax({
+    type: 'post',
+    url: '/recommend',
+    data: JSON.stringify(ratings_query),
+    contentType: 'application/json',
+    dataType: 'json',
+    success: populateResults
+  });
+}
+
 function populateResults(data) {
   $('#recommendations_list').children().remove();
   $.each(data, function(i, x) {
@@ -5,36 +30,6 @@ function populateResults(data) {
       $('#recommendations_list').append('<li><div class="recommendation_entry"><span class="recommendation_label">'+x[0]+'</span></div></li>')
     }
   })
-}
-
-function getRatings() {
-  // FIXME cleaner way to construct query?
-  var ratings_list = $('input[type=radio].first-star').map(function(i, x) {
-    movie_id = x.name.split('_')[0]
-    num_stars = $(x).data('rating').rater.children().filter('.star-rating-on').length
-    if (num_stars == 0) {
-      return [[movie_id, null]];
-    } else {
-      return [[movie_id, num_stars]]
-    }
-  });
-  ratings_query = {}
-  ratings_list.each(function(i, x) {
-    if (x[1] == null) {
-      ratings_query[x[0]] = null;
-    } else {
-      ratings_query[x[0]] = String(x[1]);
-    }
-  });
-
-  $.ajax({
-    type: 'post',
-    url: '/predict',
-    data: JSON.stringify(ratings_query),
-    contentType: 'application/json',
-    dataType: 'json',
-    success: populateResults
-  });
 }
 
 $(document).ready(function() {
